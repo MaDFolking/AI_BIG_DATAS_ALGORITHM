420516d5fdd7725a9aee87477d90dd1bbec986b8
MaDFolking <38131952+MaDFolking@users.noreply.github.com>
2018-06-30 20:32:06 +0800

    Update cifar.py

--- DeepLearningCode/pytorch/cifar.py
+++ DeepLearningCode/pytorch/cifar.py
@@ -17,27 +17,40 @@ import torchvision.datasets as datasets
 import models.cifar as models
 
 from utils import Bar, Logger, AverageMeter, accuracy, mkdir_p, savefig
-#pytorch的所有网络,这个属于参数解析模块
 
-model_names = sorted(name for name in models.__dict__
-    if name.islower() and not name.startswith("__")
-    and callable(models.__dict__[name]))
+#models是模块,里面有很多其他模块，__dict__是字典，键为属性，值为属性值,这里相当于models里的模块为键，models里模块的函数名类名为值
+#callable：检查里面是否是可被调用对象,对象即使可以被调用也可能失败，类实例化的对象肯定被调用，只要类里定义了__call__方法。
+#这里使用callable就是为了防止调用包里的函数/类对象失败。
+#sort返回是列表（相当于其他语言数组，毕竟str类型本质是列表。）
+#这个函数是我们架构的本质:通过models模块调用各个神经网路的模块，方便后面调参。
+model_names = sorted(name for name in models.__dict__ if name.islower() and not name.startswith("__") and callable(models.__dict__[name]))
 
+#argparse属于python解析命令参数,并生成帮助信息。
+#通常创建ArgumentParser对象保存所有命令行参数转为python数据类型信息.
+#常用参数:
+#(1)prop:指定程序名。通过%(prog)s引用程序名。parser.add_argument('--foo', help='foo of the %(prog)s program')
+#(2)usage : 根据参数自动生成用法信息
+#(3)description:展现程序要干什么的信息
+#其他具体信息请查看该博客: https://blog.csdn.net/guoyajie1990/article/details/76739977
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10/100 Training')
-# Datasets
-#-d 属于代号，表示dataset  --dataset是我们传进的参数，真正传入是一个-就表示代号，--表示参数名。
+
+#parser是argparse.ArgumentParser对象,add_argument用法如下:
+#- 为代号， -- 为要传递的参数 default为默认值 action为如何处理这些参数，比如action='store'为保存,='list'为保存在列表，='count'计算数目
+#我们也可以自定义action的方法，但需要继承Action类的__call__和__init__方法。 编写:class FooAction(argparse.Action): 这种格式，再写__init__和__call__方法即可。
+#nargs:保存多个参数并存放在list中。 ('--aa',nargs=2) parser("--aa',['a','b'])
+#type还可以类型转换
+#metavar 参数的名字，在显示 帮助信息时才用到
 parser.add_argument('-d', '--dataset', default='cifar10', type=str)
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
 # Optimization options
 parser.add_argument('--epochs', default=300, type=int, metavar='N',
                     help='number of total epochs to run')
-#名字一定要下划线
-parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
-parser.add_argument('--train_batch', default=128, type=int, metavar='N',
+parser.add_argument('--train-batch', default=128, type=int, metavar='N',
                     help='train batchsize')
-parser.add_argument('--test_batch', default=100, type=int, metavar='N',
+parser.add_argument('--test-batch', default=100, type=int, metavar='N',
                     help='test batchsize')
 parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
                     metavar='LR', help='initial learning rate')
@@ -48,7 +61,7 @@ parser.add_argument('--schedule', type=int, nargs='+', default=[150, 225],
 parser.add_argument('--gamma', type=float, default=0.1, help='LR is multiplied by gamma on schedule.')
 parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
                     help='momentum')
-parser.add_argument('--weight_decay', '--wd', default=5e-4, type=float,
+parser.add_argument('--weight-decay', '--wd', default=5e-4, type=float,
                     metavar='W', help='weight decay (default: 1e-4)')
 # Checkpoints
 parser.add_argument('-c', '--checkpoint', default='checkpoint', type=str, metavar='PATH',
@@ -63,7 +76,7 @@ parser.add_argument( '-a', '--arch',metavar='ARCH', default='resnet20',
                         ' (default: resnet18)')
 parser.add_argument('--depth', type=int, default=29, help='Model depth.')
 parser.add_argument('--cardinality', type=int, default=8, help='Model cardinality (group).')
-parser.add_argument('--widen_factor', type=int, default=4, help='Widen factor. 4 -> 64, 8 -> 128, ...')
+parser.add_argument('--widen-factor', type=int, default=4, help='Widen factor. 4 -> 64, 8 -> 128, ...')
 parser.add_argument('--growthRate', type=int, default=12, help='Growth rate for DenseNet.')
 parser.add_argument('--compressionRate', type=int, default=2, help='Compression Rate (theta) for DenseNet.')
 # Miscs
@@ -71,20 +84,24 @@ parser.add_argument('--manualSeed', type=int, help='manual seed')
 parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
                     help='evaluate model on validation set')
 #Device options
-parser.add_argument('--gpu_id', default='0', type=str,
+parser.add_argument('--gpu-id', default='0', type=str,
                     help='id(s) for CUDA_VISIBLE_DEVICES')
 
+#parse_args()运行时，会用'-'来认证可选参数，剩下的即为位置参数。 parser.parse_args(['BAR', '--foo', 'FOO']) => Namespace(bar='BAR', foo='FOO')
+#所以这里直接获取所有参数
 args = parser.parse_args()
+#_get_kwargs看底层代码应该是获取相关属性，并是字典方式，估计跟model的__dict__类似，上文中获取参数，这里按照字典方式存储在state中。
 state = {k: v for k, v in args._get_kwargs()}
 
 # Validate dataset
 assert args.dataset == 'cifar10' or args.dataset == 'cifar100', 'Dataset can only be cifar10 or cifar100.'
 
-# Use CUDA
+#os获取gpu，environ是获取或者设置系统变量。+=是设置 下面应该是获取。
 os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_id
+#torch框架中调用cuda是否可行。
 use_cuda = torch.cuda.is_available()
 
-# Random seed
+# 下面是args里设置随机种子方式和torch，不要忘了在cuda上也设置。
 if args.manualSeed is None:
     args.manualSeed = random.randint(1, 10000)
 random.seed(args.manualSeed)
@@ -96,8 +113,8 @@ best_acc = 0  # best test accuracy
 
 def main():
     global best_acc
-    start_epoch = args.start_epoch  # start from epoch 0 or last checkpoint epoch
-
+    start_epoch = args.start_epoch  #初始化epoch,这里是将上面参数名字有-改成_。使其规范化。
+    #这个是绝对路径，不存在则创建
     if not os.path.isdir(args.checkpoint):
         mkdir_p(args.checkpoint)
 
@@ -105,32 +122,39 @@ def main():
 
     # Data
     print('==> Preparing dataset %s' % args.dataset)
+	#trannsform是数据转化，调用了Compose类是相当于tensorflow的图结构，先创建内存空间，数据传输时再进入。
+	#这样用空间换取时间。实现类似懒加载机制。以后都用这个写法
     transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.RandomCrop(32, padding=4), #随机裁剪
+        transforms.RandomHorizontalFlip(),    #随机水平翻转
+        transforms.ToTensor(),				  #转化为矩阵
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),   #标准化，归一化
     ])
-
+	#下面是测试数据集
     transform_test = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
     if args.dataset == 'cifar10':
+		#CIFAR10类不加()是不创建对象，这样的好处是
+		#1.速度快，毕竟创建对象和销毁对象是最消耗性能
+		#2.直接调用里面的静态变量和方法
+		#3.本质是起别名，方便下面加载。
         dataloader = datasets.CIFAR10
         num_classes = 10
     else:
         dataloader = datasets.CIFAR100
         num_classes = 100
 
-
+	#这就是起别名，方便下面加载调用。
     trainset = dataloader(root='./data', train=True, download=True, transform=transform_train)
     trainloader = data.DataLoader(trainset, batch_size=args.train_batch, shuffle=True, num_workers=args.workers)
 
     testset = dataloader(root='./data', train=False, download=False, transform=transform_test)
     testloader = data.DataLoader(testset, batch_size=args.test_batch, shuffle=False, num_workers=args.workers)
 
-    # Model
+    #调用各个神经网络，通过刚才的args,通过名字来调用，里面参数用model模块的dict形成字典，然后通过里面模块属性找到相应神经网络的模块，进而赋值各个变量
+    #根据我们上面配置的参数，去调用，这里是通过arch来找到相应的模块名。
     print("==> creating model '{}'".format(args.arch))
     if args.arch.startswith('resnext'):
         model = models.__dict__[args.arch](
@@ -163,14 +187,17 @@ def main():
     else:
         model = models.__dict__[args.arch](num_classes=num_classes)
 
+    #通过torch模块的cuda并行处理
     model = torch.nn.DataParallel(model).cuda()
+    #import torch.backends.cudnn as cudnn 包中找到cudnn的benchmark基准属性，属于pytorch框架知识不做详细说明。
     cudnn.benchmark = True
     print('    Total params: %.2fM' % (sum(p.numel() for p in model.parameters())/1000000.0))
-
+    #损失值
     criterion = nn.CrossEntropyLoss()
+    #通过损失值来获取优化器，SGD格式，也是pytorch用法。不做详细。
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
 
-    # Resume
+    #上述求完损失和优化器后来通过resume加载保存到路径中,并写在日志文件上，这里我做个注释，没太明白。
     title = 'cifar-10-' + args.arch
     if args.resume:
         # Load checkpoint.
@@ -187,14 +214,14 @@ def main():
         logger = Logger(os.path.join(args.checkpoint, 'log.txt'), title=title)
         logger.set_names(['Learning Rate', 'Train Loss', 'Valid Loss', 'Train Acc.', 'Valid Acc.'])
 
-
+    #测试损失，测试目标
     if args.evaluate:
         print('\nEvaluation only')
         test_loss, test_acc = test(testloader, model, criterion, start_epoch, use_cuda)
         print(' Test Loss:  %.8f, Test Acc:  %.2f' % (test_loss, test_acc))
         return
 
-    # Train and val
+    #迭代训练
     for epoch in range(start_epoch, args.epochs):
         adjust_learning_rate(optimizer, epoch)
 
@@ -203,10 +230,10 @@ def main():
         train_loss, train_acc = train(trainloader, model, criterion, optimizer, epoch, use_cuda)
         test_loss, test_acc = test(testloader, model, criterion, epoch, use_cuda)
 
-        # append logger file
+        #日志加载
         logger.append([state['lr'], train_loss, test_loss, train_acc, test_acc])
 
-        # save model
+        #不停的迭代判断最好模型，然后获取，并用save_checkpoint来保存。
         is_best = test_acc > best_acc
         best_acc = max(test_acc, best_acc)
         save_checkpoint({
@@ -216,7 +243,7 @@ def main():
                 'best_acc': best_acc,
                 'optimizer' : optimizer.state_dict(),
             }, is_best, checkpoint=args.checkpoint)
-
+    #写完后日志要关闭。并保存
     logger.close()
     logger.plot()
     savefig(os.path.join(args.checkpoint, 'log.eps'))
@@ -224,6 +251,7 @@ def main():
     print('Best acc:')
     print(best_acc)
 
+#训练函数
 def train(trainloader, model, criterion, optimizer, epoch, use_cuda):
     # switch to train mode
     model.train()
@@ -278,7 +306,7 @@ def train(trainloader, model, criterion, optimizer, epoch, use_cuda):
         bar.next()
     bar.finish()
     return (losses.avg, top1.avg)
-
+#测试函数。
 def test(testloader, model, criterion, epoch, use_cuda):
     global best_acc
 
