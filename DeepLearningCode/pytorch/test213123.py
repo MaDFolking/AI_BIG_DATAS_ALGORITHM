3fda723782d08915ebff6e8edbca4424e6cee20a
MaDFolking <38131952+MaDFolking@users.noreply.github.com>
2018-06-30 10:07:55 +0800

    Create test213123.py

---
+++ DeepLearningCode/pytorch/test213123.py
@@ -0,0 +1,17 @@
+{
+  padding:0px;
+  margin:0px;
+  font:12px normal"micrisift yahei";
+}
+
+tabs {width:290px;padding:5px;height:150px;margin:20px;}
+
+tabs ul{list-style:none;display:block;height:30px;line-height:30px;border-bottom:2px saddlebrown solid;}
+
+tabs ul li{backgrond:#fff;cursor:pointer;float:left;list-style:none;height:28px;
+ line-height:28px;margin:0px 3px;border:1px solid #aaa;border-bottom:none;display:inline-block;
+# width:60px;text-align:center;}
+
+tabs ul li.on{border-top:2px solid saddlebrown;border-bottom:2px solid #fff;}
+
+tabs div{height:120px;line-height:25px;border:1px solid #336699;border-top:none;padding:5px;}.hide{display:none;}
